70a950666a2629d1d826a1b445888b8f885f82b1
Axel Leos <rleos1802@outlook.com>
2024-07-31 18:15:52 -0600

    background home

--- src/app/basic/pages/home-page/home-page.component.ts
+++ src/app/basic/pages/home-page/home-page.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'basic-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-
+  greetings = [  ];
 }
